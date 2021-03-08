06154de265db19a4bd4d66afb8455dcfbf765903
Wanchana Moonmuang <6210612757@student.tu.ac.th>
2021-03-08 11:01:40 +0700

    Update Main

---
+++ Main.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import IPython
+import scipy
+import sklearn
+
+img = cv.imread('images/02_g.jpg')
+plt.imshow(img)
